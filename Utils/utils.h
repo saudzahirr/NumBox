ea0ae20c9c83ae6f5a9413b81880c7ecfe723239
Saud Zahir <m.saud.zahir@gmail.com>
2023-12-21 11:35:49 +0500

    Add linear algebra utils

--- Utils/utils.h
+++ Utils/utils.h
@@ -1,10 +1,20 @@
 #ifndef UTILS
 #define UTILS
 
-// Matrix multiplication.
-double* multiplyMatrices(double** A, int rowsA, int colsA, double** B, int rowsB, int colsB);
 
-// Derivative of a function using Ab initio principle.
-double calculateDerivative(double (*f)(double), double x, double h);
+double Derivative(double (*f)(double), double x, double h);
+
+
+// Linear Algebra Utils.
+double** matrixProduct(double** A, int rowsA, int colsA, double** B, int rowsB, int colsB);
+
+double Dot(double* vectorA, double* vectorB, int n);
+
+void Cross(double* vectorA, double* vectorB, double* vector);
+
+void cleanMatrix(double** matrix, int rows);
+
+void displayMatrix(double** matrix, int rows, int cols, int cellSize);
+
 
 #endif
