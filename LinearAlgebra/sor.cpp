5c7bede8e23122ca70f2c0b88c194aa06af562f9
Saud Zahir <m.saud.zahir@gmail.com>
2023-12-21 21:06:55 +0500

    Small changes made to SOR

--- LinearAlgebra/sor.cpp
+++ LinearAlgebra/sor.cpp
@@ -20,7 +20,7 @@ using namespace std;
 double* SuccessiveOverRelaxation(double** A, double* b, double* x, double w, int n) {
     double* x_k = new double[n];
 
-    // Gauss-Seidel method elementwise formula.
+    // SOR method element-wise formula.
     for (int a = 1; a <= ITERATIONS; ++a) {
         for (int i = 0; i < n; ++i) {
             double S1 = 0;
@@ -31,15 +31,22 @@ double* SuccessiveOverRelaxation(double** A, double* b, double* x, double w, int
             for (int j = i + 1; j < n; ++j) {
                 S2 += A[i][j] * x[j];
             }
-            x_k[i] = (1 - w) * x[i] + (w / A[i][i]) * (b[i] - S1 - S2);
+
+            if (A[i][i] != 0) {
+                x_k[i] = (1 - w) * x[i] + (w / A[i][i]) * (b[i] - S1 - S2);
+            } else {
+                cerr << "Division by zero encountered. SOR method did not converge!" << endl;
+                delete[] x_k;
+                return nullptr;
+            }
         }
 
-        // Absolute error evaluation
-        double max_diff = 0.0;
+        // Relative error evaluation
+        double max_relative_diff = 0.0;
         for (int i = 0; i < n; ++i) {
-            double diff = abs(x_k[i] - x[i]);
-            if (diff > max_diff) {
-                max_diff = diff;
+            double relative_diff = abs((x_k[i] - x[i]) / x_k[i]);
+            if (relative_diff > max_relative_diff) {
+                max_relative_diff = relative_diff;
             }
         }
 
@@ -47,22 +54,22 @@ double* SuccessiveOverRelaxation(double** A, double* b, double* x, double w, int
             x[i] = x_k[i];
         }
 
-        if (max_diff < TOLERANCE) {
-            cout << "Gauss-Seidel method converged at " << a << " iterations." << endl;
+        if (max_relative_diff < TOLERANCE) {
+            cout << "SOR method converged at " << a << " iterations." << endl;
             delete[] x_k;
             return x;
         }
     }
     delete[] x_k;
 
-    cerr << "Gauss-Seidel method did not converge!" << endl;
+    cerr << "SOR method did not converge!" << endl;
     return nullptr;
 }
 
 
 int main() {
     const int n = 4;
-    const double w = 1.75;
+    const double w = 1.25;
 
     double** A = new double*[n];
 
@@ -71,11 +78,11 @@ int main() {
     }
 
     // Define a matrix (4D array)
-    A[0][0] = 4.0; A[0][1] = 2.0; A[0][2] = -6.0; A[0][3] = 0;
-    A[1][0] = 5.0; A[1][1] = -4.0; A[1][2] = 10.0; A[1][3] = 8.0;
-    A[2][0] = 0.0; A[2][1] = 9.0; A[2][2] = 4.0; A[2][3] = 2.0;
-    A[3][0] = 1.0; A[3][1] = 0.0; A[3][2] = -7.0; A[3][3] = 5.0;
-    double b[n] = {2.0, 21.0, -12.0, -6.0};
+    A[0][0] = 10.0; A[0][1] = -1.0; A[0][2] = 2.0; A[0][3] = 0;
+    A[1][0] = -1.0; A[1][1] = 11.0; A[1][2] = -1.0; A[1][3] = 3.0;
+    A[2][0] = 2.0; A[2][1] = -1.0; A[2][2] = 10.0; A[2][3] = -1.0;
+    A[3][0] = 0.0; A[3][1] = 3.0; A[3][2] = -1.0; A[3][3] = 8.0;
+    double b[n] = {6.0, 25.0, -11.0, 15.0};
     double x[n] = {0.0, 0.0, 0.0, 0.0};
 
     double* U = SuccessiveOverRelaxation(A, b, x, w, n);
@@ -86,7 +93,7 @@ int main() {
         delete[] U; // Clean up dynamic memory
     }
 
-    cleanMatrix(A, n);
+        cleanMatrix(A, n);
 
     return 0;
 }
--- LinearAlgebra/sor.h
+++ LinearAlgebra/sor.h
@@ -2,14 +2,12 @@
 #define SOR_H
 
 /**
- * @brief Successive over-relaxation (SOR)
- * 
- * This method solves a linear system of equations (Ax = b) using Gauss-Seidel algorithm.
+ * @brief Successive Over-Relaxation (SOR) method to solve a linear system of equations.
  *
  * @param A Coefficient matrix of the linear system.
  * @param b Right-hand side vector of the linear system.
  * @param x Initial guess for the solution (input) and the resulting solution (output).
- * @param w weight parameter.
+ * @param w Relaxation parameter.
  * @param n Size of the linear system.
  * @return Pointer to the converged solution vector. The memory is allocated dynamically.
  */
